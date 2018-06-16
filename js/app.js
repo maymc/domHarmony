2a2fae291d06e09b1f4be0b061ceaf8db6c3f7cb
May Chen <maychen@Mays-MacBook-Pro.local>
2018-06-15 20:04:50 -1000

    completed ex11

--- js/app.js
+++ js/app.js
@@ -53,6 +53,10 @@ firstNameClass[0].innerHTML = "Wendy";
 
 //11. In the div id matches section, target the first div element with the class name of otherAge and change Age to 48.
 
+var otherAgeClass = document.getElementsByClassName("otherAge");
+otherAgeClass[0].innerHTML = 48;
+
+
 //12. In the div id matches section, target the first div element with the class name of status and change Status to Single Mother.
 
 //13. In the div id matches section, target the second div element with the class name of firstName and change Name to Peko Chan.
